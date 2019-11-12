100935b45dc35cc84f43e5937e24bcd09c28f918
hyeongjunk05 <hyeongjunk05@gmail.com>
2019-11-12 22:44:15 +0900

    1차업로드

---
+++ js/toggleClass.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+		
+	$('#noah p span').hover(function(){
+		$('#noah div').css({'width':'190px'});
+		$('#noah div').toggleClass('hehe');
+		$('#noah div span').css({'display':'inline'});
+		$('body').css({'background-image':'url(img/background.png)', 'backgroundSize':'100%', 'backgroundRepeat':'no-repeat'});
+	},function(){
+		$('#noah div').css({'width':'250px'});
+		$('#noah div').removeClass();
+		$('#noah div span').delay(3000).fadeOut(1000,'linear');
+	});
+});
+  
