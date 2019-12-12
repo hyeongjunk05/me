7c656e48d946caeaaa747a854ed31141d496a283
Hyeongjun Kim <hyeongjunk05@gmail.com>
2019-12-12 12:00:00 +0900

    Update toggleClass.js

--- js/toggleClass.js
+++ js/toggleClass.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 		
 	$('#noah p span').hover(function(){
 		$('#noah div').css({'width':'190px'});
-		$('#noah div').stop().clearqueue().toggleClass('hehe');
+		$('#noah div').toggleClass('hehe');
 		$('#noah div span').css({'display':'inline'});
 		$('body').css({'backgroundImage':'url(img/background.jpg)', 'backgroundSize':'cover', 'backgroundRepeat':'no-repeat'});
 	},function(){
 		$('#noah div').css({'width':'250px'});
-		$('#noah div').stop().clearqueue().removeClass();
+		$('#noah div').removeClass();
 		$('#noah div span').delay(3000).fadeOut(1000,'linear');
 	});
 });
