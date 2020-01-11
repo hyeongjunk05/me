4015f30e60513548c147be1c11a7c93767b4e1f6
Hyeongjun Kim <hyeongjunk05@gmail.com>
2020-01-11 21:56:49 +0900

    Update toggleClass.js

--- js/toggleClass.js
+++ js/toggleClass.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		$('#where p a').css({'color':'#fff'})
 		$('body').css({'backgroundImage':'url(img/background.jpg)', 'backgroundSize':'cover', 'backgroundRepeat':'no-repeat'});
 	},function(){
-		$('#noah div').css({'width':'100px'});
+		$('#noah div').css({'width':'130px'});
 		$('#noah div').stop().clearQueue().removeClass();
 		$('#noah div span').delay(3000).fadeOut(1000,'linear');
 	});
